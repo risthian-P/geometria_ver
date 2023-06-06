b51c028d1e53e6b1e081c1e37e99ce595227d2c3
Cristhian Panora <cristhian.panora@gmail.com>
2023-06-06 08:56:17 -0500

    nuevos cambios

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class menuPrincipal {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        redireccion_2 miFigura=new redireccion_2(4,2);
+        miFigura.setLado(3);
+        redireccion_2 mi2figura = new redireccion_2(3,10);
+        System.out.println(miFigura.areaCuadrado(7));
+        System.out.println(mi2figura.areaCuadrado(7));
     }
 }
--- src/redireccion_2.java
+++ src/redireccion_2.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class redireccion_2 {
+
+public class redireccion_2 {
+    //triangulo equilatero y cuadrado
+    private int nLados;//número de lados
+    public float lado;//longitud del lado
+
+    //Constructor
+    public figuras2D(int nLados) {
+        this.nLados = nLados;
+    }
+
+    public figuras2D(float lado) {
+        this.lado = lado;
+    }
+
+    public figuras2D(int nLados, float lado) {
+        this.nLados = nLados;
+        this.lado = lado;
+    }
+
+    //Setters y getters
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+
+    public int getnLados() {
+        return nLados;
+    }
+
+    public void setnLados(int nLados) {
+        this.nLados = nLados;
+    }
+
+
+    public float areaCuadrado(int lado){
+        return this.lado*this.lado;
+    };
+    private float perimetroCuadrado(){
+        return 0;
+    };
+
+    private float areaTriangulo(){
+        return 0;
+
+    };
+    private float perimetroTriangulo(){
+        return 0;
+
+    };
 }
+
